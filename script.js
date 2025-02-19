3e4d6b337d5a62a7f51b18c2db75dae88aa31a30
Justdan08 <dan081998@gmail.com>
2025-02-19 02:54:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Word Search Configuration
 const gridSize = 10;
-const words = ["JAVASCRIPT", "HTML", "CSS", "WEB", "GAME", "PYTHON", "CODE", "FUN"];
+const words = ["HULK", "THOR", "IRONMAN", "GROOT", "ROCKET", "WIDOW", "HAWKEYE", "ANTMAN", "BUCKY", "TESSERACT", "GAUNTLET", "SHEILD", "VIBRANIUM",];
 let selectedCells = [];
 let foundWords = [];
 let isDragging = false;
